ee1f047efe785fecb3a70680173edb2ad962df87
Colyn <colyn.brown@gmail.com>
2018-08-05 17:52:32 -0700

    this plugin does not require hooks; loading env vars in constructor

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const dotenv = require('dotenv');
-const dotenvExpand = require('dotenv-expand');
-const chalk = require('chalk');
+const dotenv = require('dotenv')
+const dotenvExpand = require('dotenv-expand')
+const chalk = require('chalk')
 
 class ServerlessPlugin {
   constructor(serverless, options) {
-    this.serverless = serverless;
-    this.env = {};
-    this.serverless.service.provider.environment = this.serverless.service.provider.environment || {};
-    this.hooks = {
-      "before:offline:start:init": this.loadEnv.bind(this),
-      "before:offline:start": this.loadEnv.bind(this),
-      "before:invoke:local:invoke": this.loadEnv.bind(this),
-      "before:deploy:resources": this.loadEnv.bind(this),
-      "before:deploy:functions": this.loadEnv.bind(this),
-      "before:package:initialize": this.loadEnv.bind(this),
-    }
+    this.serverless = serverless
+    this.env = {}
+    this.serverless.service.provider.environment =
+      this.serverless.service.provider.environment || {}
+    this.loadEnv()
   }
 
   loadEnv() {
     try {
-      this.serverless.cli.log('DOTENV: Loading environment variables:');
-      var config = this.serverless.service.custom && this.serverless.service.custom['dotenv'];
-      var envPath = (config && config.path) || '.env';
-      this.env = dotenvExpand(dotenv.config({path: envPath})).parsed;
+      var config =
+        this.serverless.service.custom &&
+        this.serverless.service.custom['dotenv']
+      var envPath = (config && config.path) || '.env'
+      this.env = dotenvExpand(dotenv.config({ path: envPath })).parsed
 
-      var include = false;
+      var include = false
       if (config && config.include) {
-        include = config.include;
+        include = config.include
       }
-      if (include) {
-        Object.keys(this.env)
-          .filter((key) => !include.includes(key))
-          .forEach((key) => {
-            delete this.env[key]
-          })
-      }
-      Object.keys(this.env)
-        .forEach((key) => {
-          this.serverless.cli.log("\t - " + key);
-          this.serverless.service.provider.environment[key] = this.env[key];
+
+      if (this.env) {
+        this.serverless.cli.log('DOTENV: Loading environment variables:')
+        if (include) {
+          Object.keys(this.env)
+            .filter(key => !include.includes(key))
+            .forEach(key => {
+              delete this.env[key]
+            })
+        }
+        Object.keys(this.env).forEach(key => {
+          this.serverless.cli.log('\t - ' + key)
+          this.serverless.service.provider.environment[key] = this.env[key]
         })
+      } else {
+        this.serverless.cli.log('DOTENV: Could no find .env file.')
+      }
     } catch (e) {
-        console.error(chalk.red('\n Serverless Plugin Error --------------------------------------\n'))
-        console.error(chalk.red('  ' + e.message));
+      console.error(
+        chalk.red(
+          '\n Serverless Plugin Error --------------------------------------\n'
+        )
+      )
+      console.error(chalk.red('  ' + e.message))
     }
-
   }
-
 }
 
-module.exports = ServerlessPlugin;
+module.exports = ServerlessPlugin
