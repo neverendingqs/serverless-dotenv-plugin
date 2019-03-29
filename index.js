d5f3130ad75cde4a9bdff64071ef6b841ad55bad
Harmen Janssen <harmen@grrr.nl>
2019-03-29 12:32:33 +0100

    Revert "Make sure variables are resolved in configuration"
    This reverts commit 6ea1f21b43a0f12322a285db40c1b5fee524eb97.

--- index.js
+++ index.js
@@ -7,30 +7,13 @@ const fs = require('fs')
 
 class ServerlessPlugin {
   constructor(serverless, options) {
-    this.options = options
     this.serverless = serverless
     this.serverless.service.provider.environment =
       this.serverless.service.provider.environment || {}
     this.config =
       this.serverless.service.custom && this.serverless.service.custom['dotenv']
 
-    this.commands = {
-      deploy: {
-        lifecycleEvents: [
-          'package'
-        ]
-      },
-    }
-    this.hooks = {
-      'before:package:initialize': this.beforeDeploy.bind(this)
-    }
-  }
-
-  beforeDeploy() {
-    this.config =
-      this.serverless.service.custom && this.serverless.service.custom['dotenv']
-
-    this.loadEnv(this.getEnvironment(this.options))
+    this.loadEnv(this.getEnvironment(options))
   }
 
   getEnvironment(options) {
@@ -78,10 +61,9 @@ class ServerlessPlugin {
         this.serverless.service.provider.environment[key] = envVars[key]
       })
     } else {
-      throw new Error(`DOTENV: Could not find ${envFileName} file.`)
+      throw new Error('DOTENV: Could not find .env file.')
     }
   }
 }
 
 module.exports = ServerlessPlugin
-
