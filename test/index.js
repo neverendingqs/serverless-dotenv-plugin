b19f15f3cc083204ad32b939dff7371f4bacce8d
Mark Tse <mark.tse@neverendingqs.com>
2020-10-15 21:45:22 -0400

    test: adding proxyquire to stub deps.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,28 @@
 const chai = require('chai')
+const proxyquire = require('proxyquire')
 const should = chai.should()
 const sinon = require('sinon')
 
-const ServerlessPlugin = require('../')
+chai.use(require('sinon-chai'))
 
 describe('ServerlessPlugin', function () {
   beforeEach(function () {
     this.sandbox = sinon.createSandbox()
+
+    this.requireStubs = {
+      chalk: {
+        red: this.sandbox.stub(),
+      },
+      dotenv: {
+        config: this.sandbox.stub(),
+      },
+      dotenvExpand: this.sandbox.stub(),
+      fs: {
+        existsSync: this.sandbox.stub(),
+      },
+    }
+
+    this.ServerlessPlugin = proxyquire('../', this.requireStubs)
 
     this.serverless = {
       cli: {
@@ -18,7 +34,7 @@ describe('ServerlessPlugin', function () {
     }
     this.options = {}
 
-    this.plugin = new ServerlessPlugin(this.serverless, this.options)
+    this.plugin = new this.ServerlessPlugin(this.serverless, this.options)
   })
 
   afterEach(function () {
@@ -28,6 +44,25 @@ describe('ServerlessPlugin', function () {
   describe('constructor()', function () {
     it('does not err out on minimal configuration', function () {
       should.exist(this.plugin)
+    })
+
+    it('loads environment variables as expected', function () {
+      const env = 'unittests'
+
+      const getEnvironment = this.sandbox.stub(
+        this.ServerlessPlugin.prototype,
+        'getEnvironment',
+      )
+      getEnvironment.withArgs(this.options).returns(env)
+
+      const loadEnv = this.sandbox.stub(
+        this.ServerlessPlugin.prototype,
+        'loadEnv',
+      )
+
+      new this.ServerlessPlugin(this.serverless, this.options)
+
+      loadEnv.should.have.been.calledWith(env)
     })
   })
 
