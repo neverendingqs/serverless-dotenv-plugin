3f5ff4dcf44a8cdb913168d00463e2d958446a6d
Mark Tse <mark.tse@neverendingqs.com>
2021-03-13 18:05:25 -0500

    chore: prettier.

--- examples/simple-alternative-example/.serverless_plugins/serverless-env-var-logger.js
+++ examples/simple-alternative-example/.serverless_plugins/serverless-env-var-logger.js
@@ -14,4 +14,4 @@ module.exports = class ServerlessEnvVarLogger {
   log() {
     console.log({ FOO: process.env.FOO });
   }
-}
+};
--- examples/simple-alternative-example/configs.js
+++ examples/simple-alternative-example/configs.js
@@ -4,4 +4,4 @@ module.exports = async ({ options, resolveConfigurationProperty }) => {
   // Load env var into Serverless environment
   const envVars = dotenv.config({ path: '.env.local' }).parsed;
   return envVars;
-}
+};
