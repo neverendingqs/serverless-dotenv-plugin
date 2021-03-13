deb913f4eb9ec5f8a40e75c6bfb6a5d2e67ed855
Mark Tse <mark.tse@neverendingqs.com>
2021-03-13 18:18:11 -0500

    docs: add comment about more complicated workflows.

--- examples/simple-alternative-example/configs.js
+++ examples/simple-alternative-example/configs.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 
 module.exports = async ({ options, resolveConfigurationProperty }) => {
   // Load env vars into Serverless environment
+  // You can do more complicated env var resolution with dotenv here
   const envVars = dotenv.config({ path: '.env.local' }).parsed;
   return envVars;
 };
